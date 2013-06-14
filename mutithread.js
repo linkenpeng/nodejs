315add079c04a5d17935c94b075af39c3f2aba9a
linkenpeng <collin_linken@qq.com>
2013-06-14 23:52:53 +0800

    some

--- mutithread.js
+++ mutithread.js
@@ -28,19 +28,37 @@ var titleReg = /<a[^\s]+/gi; //new RegExp('h3');
 		path : '/s?word=多线程&ie=utf-8',
 		method : 'GET'
 	};
-
+	var fileName = 't.html';
 	http.get(options, function(res) {
-		console.log('STATUS: ' + res.statusCode);
-		console.log('HEADERS: ' + JSON.stringify(res.headers));
+		//console.log('STATUS: ' + res.statusCode);
+		//console.log('HEADERS: ' + JSON.stringify(res.headers));
 		res.setEncoding('utf8');
+		try {
+			fs.unlink(fileName, function(){});
+		} catch(e) {
+			throw e;
+		}
+		var i = 0;
+		console.log(i);
 		res.on('data', function (data) {
-			fs.appendFile('t.html',data,function(err){
+			i++;
+			/*
+			fs.appendFile(fileName, data, function(err){
 				if(err) throw err;
 				console.log('save ok');
 			});
+			
+			console.log(data);
 			var titleArr = data.match(titleReg);
 			console.log(titleArr);
+			*/
+			console.log(i);
 		});
+		
+		res.on('end', function(){
+			
+		});
+		console.log(i);
 	});
 //}
 
